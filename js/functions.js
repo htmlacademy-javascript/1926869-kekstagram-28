af699600c64e7996ce3f521c7a0dc9a01408e3ee
Виктор Филин <filinn14@gmail.com>
2023-02-28 22:24:23 +0300

    fix: eslint

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,4 @@
 const isStringLength = (string, number) => string.length === number;
-console.log(isStringLength('проверка', 8));
-
-
 
 
 const isPalindrome = (string) => {
@@ -13,18 +10,6 @@ const isPalindrome = (string) => {
   const newString = string.split('').reverse().join('');
   return string === newString;
 };
-console.log(isPalindrome('aBcd D cb a'));
-
-
-
-
-
-const isCrunch = (data) => typeof data === 'number' ? data : data.replace(/\D/g,'');
-console.log(isCrunch(1233));
-console.log(isCrunch('Пр123и вет123'))
-
-
-
 
 
 const getNewString = (origin, len, addStr) => {
@@ -36,5 +21,3 @@ const getNewString = (origin, len, addStr) => {
   }
   return origin;
 };
-
-console.log(getNewString('q', 4, 'we'));
